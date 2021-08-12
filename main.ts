a7300ee3e81f52bcc5e60f5be04c59c507598caf
kakachielotaku <88722157+kakachielotaku@users.noreply.github.com>
2021-08-12 08:27:57 -0500

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,12 @@
+let segundos = 0
+segundos += 1
+basic.forever(function () {
+    let minutos = 0
+    if (segundos >= minutos) {
+        basic.showNumber(segundos)
+        basic.pause(100)
+        segundos += 59
+    } else {
+    	
+    }
+})
