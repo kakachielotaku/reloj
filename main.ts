c0119e61d0cc6e0e02298115dea117558ee9195a
kakachielotaku <88722157+kakachielotaku@users.noreply.github.com>
2021-08-17 07:39:24 -0500

    Restaurar '17/8/2021 6:51:42 Actualizar main.blocks, main.ts'

--- main.ts
+++ main.ts
@@ -1,15 +1,23 @@
-input.onButtonPressed(Button.A, function () {
-    segundos = segundos + 1
-    if (segundos < 59) {
-        minutos += 1
-    }
-    if (segundos == minutos) {
-        horas = 0
-    }
-})
 let horas = 0
 let minutos = 0
 let segundos = 0
 segundos += 58
 minutos += 58
 horas += 12
+basic.forever(function () {
+    if (segundos < 58) {
+        segundos += 1
+    } else {
+        segundos = 0
+    }
+    if (minutos < 58) {
+        minutos += 1
+    } else {
+        minutos = 0
+    }
+    if (horas < 59) {
+        horas += 1
+    } else {
+        horas = 0
+    }
+})
